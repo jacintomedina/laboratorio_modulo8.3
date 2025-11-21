64fd2df91cab1adbe6f39522ba97f971ad33a5e9
jmedina <jmedinalba@gmail.com>
2025-11-21 21:18:23 +0100

    primer commit poc1

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,20 @@
-import "./style.css";
+function barajarCartas(carta: any): void {
+  // Inicializamos un índice 'currentIndex' en la longitud del array.
+  let currentIndex = carta.length;
+  let randomIndex: number;
 
-console.log("Hello Typescript!");
+  // Mientras queden elementos a barajar.
+  while (currentIndex !== 0) {
+    // Elegimos un elemento restante al azar.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // Intercambiamos el elemento actual con el elemento elegido al azar.
+    [carta[currentIndex], carta[randomIndex]] = [
+      carta[randomIndex],
+      carta[currentIndex],
+    ];
+  }
+
+  return carta;
+}
