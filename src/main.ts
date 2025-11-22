b398ab8397ccf2c364ebfabf2a74ffc78d7d4e77
jmedina <jmedinalba@gmail.com>
2025-11-22 11:10:52 +0100

    primer commit poc4

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,18 @@
-import "./style.css";
+const containerCarta = document.querySelectorAll(".container-carta");
 
-console.log("Hello Typescript!");
+containerCarta.forEach((carta) => {
+  const voltearCarta = (): void => {
+    const imagenCarta = carta.querySelector(".imagen-carta");
+
+    if (
+      carta instanceof HTMLElement &&
+      imagenCarta instanceof HTMLImageElement
+    ) {
+      imagenCarta.classList.toggle("oculto");
+      carta.classList.toggle("bg-frontal");
+      carta.classList.toggle("bg-reves");
+    }
+  };
+  if (carta instanceof HTMLElement)
+    carta.addEventListener("click", voltearCarta);
+});
